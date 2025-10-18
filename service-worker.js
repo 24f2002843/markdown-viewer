c88e4064659c3d7947f7b73bc10bfeae46a0997b
24f2002843 <24f2002843@ds.study.iitm.ac.in>
2025-10-18 11:16:43 +0530

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// This is a stub for the service worker. Implement caching and offline capabilities as needed.
+self.addEventListener('install', (event) => {
+    console.log('Service worker installing...');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activating...');
+});
